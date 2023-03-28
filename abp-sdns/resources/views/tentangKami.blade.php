94af5c57aa4bbdbdb7988363a3157fab224bfd01
yunolva <73276435+yunolva@users.noreply.github.com>
2023-03-28 21:44:31 +0700

    Create tentangKami.blade.php

---
+++ abp-sdns/resources/views/tentangKami.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Tentang Kami</title>
+
+</head>
+<body>
+    <!--- Start of Hamburger Menu -->
+    <div class="tentang-kami">
+        <div class="hamburger-menu">
+            <input id="menu__toggle" type="checkbox" />
+            <label class="menu__btn" for="menu__toggle">
+                <img src="./img/BurgerMenu.png" alt="BM">
+            </label>
+        
+            <ul class="menu__box" style="text-align: center;">
+              <li><a class="menu__item" text-center="#">TENTANG KAMI</a></li>
+              <li><a class="menu__item" href="#">KOPIPEDIA</a></li>
+              <li><a class="menu__item" href="#">EZ PZ KOPI</a></li>
+              <li><a class="menu__item" href="#">KONTAK</a></li>
+              <li><a class="menu__item" href="#">KERANJANG</a></li>
+              <li><a class="menu__item" href="#">AKUN SAYA</a></li>
+            </ul>
+          </div>
+    </div>
+    <!-- End of Hamburger Menu -->
+
+
+</body>
+</html>
